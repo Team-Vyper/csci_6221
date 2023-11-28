ba398e471598c22e90bc319a79f7f0af45a3b629
Chandrika2707 <89798896+Chandrika2707@users.noreply.github.com>
2023-11-28 15:01:41 -0800

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,42 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/VYOmIbdCkiUi-rBtRPssI4u_rsZOaJ9-",
+      accounts: ["9da0ebc49a276d6b24b48da749e4db86f870c6df462a917e4384c6c230d3e84b"]
+    },
+    matic: {
+      url: "https://polygon-mainnet.g.alchemy.com/v2/nAhiCHKvZkhkp4A7PkkCIBON0-BXW26d",
+      //accounts: [process.env.privateKey]
+    },
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/vjdUBy_hc3XQk--WduI9bzEEzExCRd0i",
+      accounts: [ "0028723e44d2b28c9caa318cb96722f1e87c700ee30c0942550f3492b9ed4aea" ]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
